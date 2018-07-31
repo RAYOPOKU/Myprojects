a96f20c7327c00792f26c98463d96f74b58773a2
RAYOPOKU <38057646+RAYOPOKU@users.noreply.github.com>
2018-07-31 16:26:00 -0500

    Add files via upload

---
+++ 11 hour practice/11 hour Practice 14 June .py
@@ -0,0 +1,13 @@
+#Created by me
+#The intial piece of coding, the evergreen Hello World
+print("Hello World!") 
+
+print('Hickory Dickory Dock! The mouse ran up the clock') 
+
+print("I am the best at what I do")
+
+print("Don\'t go there")
+
+print("here is a string" + " here is another string")
+
+
